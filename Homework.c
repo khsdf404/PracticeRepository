85221d83b40b533ea2999a569132b02a6f4e9612
arst0tzka <kaarst@ya.ru>
2022-02-17 16:37:36 +0300

    created dynamic array

--- Homework.c
+++ Homework.c
@@ -1,1 +1,20 @@
-asd
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+ 
+ 
+
+int main() {
+    system("chcp 1251");
+
+
+    int *dynamicList;
+    dynamicList = (int*)malloc(5 * sizeof(int));
+    int dynamicSize = 5;  
+     
+
+
+	system("pause");
+	return 0;
+}
+
