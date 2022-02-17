daba2cca18604b0e18df4e05149f6e9edcd192d2
arst0tzka <kaarst@ya.ru>
2022-02-17 16:40:32 +0300

    show array func + step back func

--- Homework.c
+++ Homework.c
@@ -8,7 +8,30 @@ void PrintMenu() {
     printf("2. Перезаписать массив полностью.\n");
     printf("3. Вывести массив.\n");
     printf("7. Выйти.\n");
+}
+void StepBack() {
+    char stepBack; 
+    printf("\nВведите 1 чтобы вернуться\n");  
+    while (1) { 
+        scanf("%s", &stepBack);  
+        if (stepBack == '1')
+            break; 
+    } 
+}
+
+
+void DynamicPrintArray(int* mas, int* dynamicSize) {
+    printf("Массив: \n[ "); 
+    for (int i = 0; i < *dynamicSize; i++) {
+        printf("%d", *(mas + i));
+        if (i + 1 != *dynamicSize)
+            printf(", ");
+    }
+    printf(" ]\n");
 } 
+
+
+
 
 void DynamicMenu(int* mas, int* dynamicSize) {
     system("cls");
@@ -38,12 +61,11 @@ void DynamicMenu(int* mas, int* dynamicSize) {
             return;
         }
     }
-    // DynamicPrintArray(mas, dynamicSize);
-    // printf("\n");
-    // StepBack();
-    // DynamicMenu(mas, dynamicSize);
+    DynamicPrintArray(mas, dynamicSize);
+    printf("\n");
+    StepBack();
+    DynamicMenu(mas, dynamicSize);
 }
-
 
 int main() {
     system("chcp 1251");
