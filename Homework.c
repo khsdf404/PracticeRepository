0a7dcb17575bcdcd529b372de68e3c36af8cbb6c
arst0tzka <kaarst@ya.ru>
2022-02-28 17:06:04 +0300

    2 typedefs + funcs: CreateArray(int len), ReallocArray(Array arr_s), DeleteArray(Array arr_s);

--- Homework.c
+++ Homework.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <malloc.h>
  
+typedef int* arr_p; 
+typedef struct Array {
+    arr_p ptr;
+    int size;
+} Array;
 
 void PrintMenu() {
-    printf("1. Вставить элемент в массив\n");
+    printf("1. Вставить элемент в конец массива\n");
     printf("2. Перезаписать массив полностью.\n");
-    printf("3. Вывести массив.\n");
+    printf("3. Вывести массив на экран.\n");
     printf("4. Удалить элемент по значению.\n");
     printf("5. Удалить элемент по индексу.\n");
     printf("6. Выйти.\n");
@@ -21,22 +26,39 @@ void StepBack() {
     } 
 }
 
+Array CreateArray(int len) {
+    Array thisArr;
+    int* dynamicArr = (int*)malloc(len * sizeof(int));
+    thisArr.ptr = dynamicArr;
+    thisArr.size = len;
+    for (int i = 0; i < len; i++) *(dynamicArr + i) = 0;
+    return thisArr;
+}
+void ReallocArray(Array arr_s) {
+    int* tmp_ptr = (int*)realloc(arr_s.ptr, arr_s.size * sizeof(int));
+    if (tmp_ptr != NULL) arr_s.ptr = tmp_ptr;
+}
+void DeleteArray(Array arr_s) {
+    free(arr_s.ptr);
+}
 
-void DynamicPrintArray(int* mas, int* dynamicSize) { 
-    printf("Массив: \n[ "); 
-    for (int i = 0; i < *dynamicSize; i++) {
-        printf("%d", *(mas+i));
-        if (i + 1 != *dynamicSize)
+/*
+void PrintArray(struct Arr arr_s) {
+    printf("Массив: [ \n\t"); 
+    for (int i = 0; i < arr_s.size; i++) {
+        printf("%d", *(arr_s.ptr + i));
+        if (i + 1 != arr_s.size)
             printf(", ");
     }
-    printf(" ]\n");
+    printf(" ];\n");
 } 
-void DynamicPushBack(int* mas, int* dynamicSize) {
+void DynamicPushBack(struct Arr arr_s) {
     printf("Введите значение\n"); 
     int newValue;
-    *dynamicSize = *dynamicSize + 1; 
+    arr_s.size++;
     scanf("%d", &newValue);
-    *(mas+*dynamicSize-1) = newValue;
+    ReallocArray(arr_s);
+    *(arr_s.ptr + arr_s.size - 1) = newValue;
 }
 void DynamicChangeArray(int* mas, int* dynamicSize) {
     int newLen = -1; 
@@ -89,7 +111,9 @@ void DynamicDeleteByIndex(int* mas, int* dynamicSize) {
     // DynamicPrintArray(mas, dynamicSize);
 }
 
-void DynamicMenu(int* mas, int* dynamicSize) {
+*/
+
+void DynamicMenu(Array arr_s) {
     system("cls");  
     PrintMenu();
     char option;
@@ -97,15 +121,15 @@ void DynamicMenu(int* mas, int* dynamicSize) {
     system("cls"); 
     switch (option) {
         case('1'): {
-            DynamicPushBack(mas, dynamicSize);
+            // DynamicPushBack(arr_s);
             break;
         }
         case('2'): {
-            DynamicChangeArray(mas, dynamicSize);
+            // DynamicChangeArray(arr_s);
             break;
         }
         case('3'): {
-            DynamicPrintArray(mas, dynamicSize);
+            // DynamicPrintArray(arr_s);
             printf("\n");
             int stepBack;
             printf("\nВведите 1 чтобы вернуться\n");
@@ -117,11 +141,11 @@ void DynamicMenu(int* mas, int* dynamicSize) {
             break;
         } 
         case('4'): {
-            DynamicDeleteByValue(mas, dynamicSize);
+            // DynamicDeleteByValue(mas, dynamicSize);
             break;
         }
         case('5'): {
-            DynamicDeleteByIndex(mas, dynamicSize);
+            // DynamicDeleteByIndex(mas, dynamicSize);
             break;
         }
         case('6'): {
@@ -133,18 +157,24 @@ void DynamicMenu(int* mas, int* dynamicSize) {
             return;
         }
     } 
-    
-    DynamicMenu(mas, dynamicSize);
+    DynamicMenu(arr_s);
 }
+
+
+
+
+
 
 int main() {
     system("chcp 1251");
+    system("cls");
 
+    Array arr_s = CreateArray(3);
 
-    int *dynamicList;
-    dynamicList = (int*)malloc(0 * sizeof(int));
-    int dynamicSize = 0;   
-    DynamicMenu(dynamicList, &dynamicSize);
+    for (int i = 0; i < arr_s.size; i++)
+        printf("%d\n", * (arr_s.ptr + i)); 
+     
+    DynamicMenu(arr_s);
 
 
 	system("pause");
