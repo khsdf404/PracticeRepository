ecbbe8496609581cf5a21a72d7ff2f8cd54dad84
arst0tzka <kaarst@ya.ru>
2022-03-18 12:36:14 +0300

    idk useless commit

--- Stack.c
+++ Stack.c
@@ -6,199 +6,99 @@
 
 #define WHITE 15 
 #define DARKGREY 8
+#define MALLOC_ERROR printf("\n  Error: malloc was finished with NULL ptr!\n")
 
 typedef int* arr_p;
 typedef struct Array {
     arr_p ptr;
     int size;
-} Array;
-typedef Array* ArrayPtr;
+} Stack;
+typedef Stack* StackPtr;
 
 
 // FoolProof get int func
 int  ScanInt(int* valuePtr) {
     int scanCount = scanf("%d", valuePtr);
-    if (scanCount) return 1;
-    getc(stdin) != '\n';
-    return 0;
+    if (scanCount) return 1; 
+    return getc(stdin) != '\n';
 }
 
 
 // Dynamic's array funcs
-Array CreateArray(int size) {
-    Array thisArr;
+Stack CreateStack(int size) {
+    Stack thisStack;
     int* dynamicArr = (int*)calloc(size * sizeof(int), sizeof(int));
-    thisArr.ptr = dynamicArr;
+    thisStack.ptr = dynamicArr;
     if (dynamicArr == NULL) {
-        thisArr.size = -1;
-        return thisArr;
+        thisStack.size = -1;
+        return thisStack;
     }
-    thisArr.size = size;
-    return thisArr;
+    thisStack.size = size;
+    return thisStack;
 }
-int ReallocArray(ArrayPtr arr, int newSize) {
-    int* tmp_ptr = (int*)realloc(arr->ptr, newSize * sizeof(int));
+int ReallocStack(StackPtr stack, int newSize) {
+    int* tmp_ptr = (int*)realloc(stack->ptr, newSize * sizeof(int));
     if (tmp_ptr != NULL || newSize == 0) {
-        arr->ptr = tmp_ptr;
-        arr->size = newSize;
+        stack->ptr = tmp_ptr;
+        stack->size = newSize;
     }
-    return (tmp_ptr != NULL || arr->size == 0);
+    return (tmp_ptr != NULL || stack->size == 0);
 }
-void DeleteArray(ArrayPtr arr) {
-    free(arr->ptr);
+void DeleteStack(StackPtr stack) {
+    free(stack->ptr);
 }
 
+void PopTop(StackPtr stack) {
+    // {1,2,3,4}  
+    printf("\n  %d", stack->ptr[stack->size - 1]);
+    for (int i = 0; i < stack->size; i++) {
+        stack->ptr[i] = stack->ptr[i+1];
+    }
+    ReallocStack(stack, stack->size - 1); // check errors 
+}
+void PushTop(StackPtr stack) {
+    int newValue = 2; // getValue func
+    ReallocStack(stack, stack->size + 1); // check errors  
+    for (int i = 0; i < stack->size; i++) {
+        stack->ptr[i + 1] = stack->ptr[i];
+    }
+    stack->ptr[0] = newValue;
+}
 
-// 4.
-void PrintArrayPart(ArrayPtr arr, int start, int end) {
-    for (int k = start; k < end; k++) {
-        printf("%d", arr->ptr[k]);
-        if (k != end - 1)
-            printf(", ");
-    }
+void FreeStack(StackPtr stack) {
+    printf("\n  {");
+    while (stack->size > 0) {
+        printf("%d, ", stack->ptr[0]);
+        PopTop(stack);
+    } 
+    printf(" }"); 
 }
-void PrintArray(ArrayPtr arr) {
-    printf("  Массив: [ ");
-    PrintArrayPart(arr, 0, arr->size);
-    printf(" ];\n");
-}
-// 1.
-void PushBack(ArrayPtr arr) {
-    if (!ReallocArray(arr, arr->size + 1)) {
-        printf("Error: realloc finished with NULL ptr!\n");
-        return;
-    }
-    int newValue;
-    printf("\n  Введите значение: ");
-    while (ScanInt(&newValue) == 0) {
-        system("cls");
-        printf("\n  Введите значение: ");
-    };
-    arr->ptr[arr->size - 1] = newValue;
-    PrintArray(arr);
-}
-// 2.
-int  GetNewSize() {
-    int newSize = -1;
-    printf("\n  Введите длину массива: ");
-    while (ScanInt(&newSize) == 0 || newSize < 0) {
-        system("cls");
-        printf("\n  Введите корректную длину массива: ");
-    };
-    return newSize;
-}
-void ChangeArray(ArrayPtr arr) {
-    int newSize = GetNewSize();
-    system("cls");
-    if (!ReallocArray(arr, newSize) && newSize != 0) {
-        printf("Error: realloc finished with NULL ptr!\n");
-        return;
-    }
-    for (int i = 0; i < newSize; i++) {
-        system("cls");
-        int nextValue;
-        printf("\n  Введите %d чисел: ", (newSize - i));
-        PrintArrayPart(arr, 0, i);
-        if (i != 0) printf(", ");
-        while (ScanInt(&nextValue) == 0) {
-            system("cls");
-            printf("\n  Введите %d чисел: ", (newSize - i));
-            PrintArrayPart(arr, 0, i);
-            if (i != 0) printf(", ");
-        };
-        arr->ptr[i] = nextValue;
-    }
-    system("cls");
-    printf("\n  Введённые числа: ");
-    PrintArrayPart(arr, 0, newSize);
-    printf("\n");
-    PrintArray(arr);
-}
-// 3.
-int  GetRandomValue(int edge, int chance) {
-    int negativeChance = rand() % 100;
-    int rndValue = rand() % edge;
-    if (negativeChance >= chance)
-        rndValue *= -1;
-    return rndValue;
-}
-void RandomArray(ArrayPtr arr) {
-    int newSize = GetNewSize();
-    if (!ReallocArray(arr, newSize) && newSize != 0) {
-        printf("Error: realloc finished with NULL ptr!\n");
-        return;
-    }
-    for (int i = 0; i < newSize; i++)
-        arr->ptr[i] = GetRandomValue(13, 70);
-    PrintArray(arr);
-}
-// 5 & 6.
-void RemoveByValue(ArrayPtr arr) {
-    int value;
-    printf("\n");
-    PrintArray(arr);
-    printf("\n  Введите значение, элементы с которым хотите удалить: ");
-    while (ScanInt(&value) == 0) {
-        system("cls");
-        printf("\n");
-        PrintArray(arr);
-        printf("\n  Введите значение, элементы с которым хотите удалить: ");
-    };
-    int offset = 0;
-    for (int i = 0; i < arr->size; i++)
-        if (arr->ptr[i] == value)
-            offset++;
-    if (!ReallocArray(arr, arr->size - offset) && arr->size != 0) {
-        printf("Error: realloc finished with NULL ptr!\n");
-        return;
-    }
-    offset = 0;
-    for (int i = 0; i < arr->size - offset; i++) {
-        arr->ptr[i] = arr->ptr[i + offset];
-        if (arr->ptr[i] == value) {
-            offset++;
-            i--;
-        }
-    }
-    printf("  Удалено элементов: %d\n", offset);
 
-    PrintArray(arr);
+int CheckSize(int stackSize) {
+    Stack tempStack = CreateStack(stackSize);
+    if (stackSize == 0)
+        return 0;
+    PopTop(tempStack);
+    if (tempStack.size == 0)
+        return 1;
 }
-void RemoveByIndex(ArrayPtr arr) {
-    int index;
-    printf("\n");
-    PrintArray(arr);
-    printf("  Введите индекс, по которому удалится элемент массива: ");
-    while (ScanInt(&index) == 0 || !(index >= 0 && arr->size > index)) {
-        system("cls");
-        printf("\n");
-        PrintArray(arr);
-        printf("  Введите индекс, по которому удалится элемент массива: ");
-    };
-    if (!ReallocArray(arr, arr->size - 1) && arr->size != 0) {
-        printf("Error: realloc finished with NULL ptr!\n");
-        return;
-    }
-    for (int k = index; k < arr->size; k++)
-        arr->ptr[k] = arr->ptr[k + 1];
-
-    printf("\n");
-    PrintArray(arr);
+void SwapTop(StackPtr stack) { 
+    if ()
 }
 
 // General funcs
-void PrintMenu(ArrayPtr arr) {
+void PrintMenu(StackPtr arr) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     printf("\n");
-    printf("  1. Вставить элемент в конец массива\n");
-    printf("  2. Перезаписать массив вручную.\n");
-    printf("  3. Перезаписать массив рандомно.\n");
-    if (arr->size == 0) SetConsoleTextAttribute(hConsole, DARKGREY);
-    printf("  4. Вывести массив на экран.\n");
-    printf("  5. Удалить элемент по индексу.\n");
-    printf("  6. Удалить элемент по значению.\n");
-    if (arr->size == 0) SetConsoleTextAttribute(hConsole, WHITE);
+    printf("  1. Очистить стек.\n");
+    printf("  2. Добавить элемент в стек.\n");
+    printf("  3. Извлечь из стека верхушку.\n");
+    // if (arr->size == 0) SetConsoleTextAttribute(hConsole, DARKGREY);
+    printf("  4. Поменять местами два верхних элемента.\n");
+    printf("  5. Удалить нижний элемент стека.\n");
+    printf("  6. Поменять местами верхний и нижний элементы стека.\n");
+    // if (arr->size == 0) SetConsoleTextAttribute(hConsole, WHITE);
     printf("  7. Выйти.\n");
     printf("\n  ");
 }
@@ -207,7 +107,7 @@ void StepBack() {
     getch();
 }
 // Menu
-void Menu(ArrayPtr arr) {
+void Menu(StackPtr arr) {
     while (1) {
         system("cls");
         PrintMenu(arr);
@@ -215,32 +115,23 @@ void Menu(ArrayPtr arr) {
         scanf("%s", &option);
         system("cls");
         switch (option) {
-        case('1'): {
-            PushBack(arr);
-            break;
+        case('1'): { 
+            continue;
         }
-        case('2'): {
-            ChangeArray(arr);
-            break;
+        case('2'): { 
+            continue;
         }
-        case('3'): {
-            RandomArray(arr);
-            break;
+        case('3'): { 
+            continue;
         }
         case('4'): {
-            printf("\n");
-            if (arr->size != 0)
-                PrintArray(arr);
+            printf("\n"); 
             continue;
         }
-        case('5'): {
-            if (arr->size != 0)
-                RemoveByIndex(arr);
+        case('5'): { 
             continue;
         }
-        case('6'): {
-            if (arr->size != 0)
-                RemoveByValue(arr);
+        case('6'): { 
             continue;
         }
         case('7'): {
@@ -261,20 +152,37 @@ int main() {
     system("chcp 1251");
     system("cls");
 
-    Array arr = CreateArray(0);
-    if (arr.size == -1) {
-        printf("Error: malloc was finished with NULL ptr!\n");
-        system("pause");
-        DeleteArray(&arr);
-        return;
-    }
-
-
-    Menu(&arr);
-
-
+    Stack s1 = CreateStack(10);
+    Stack s2 = s1;
+    s1.size = 8;
+    printf("\n %d \n", (&s1.ptr[0]));
+    printf("\n %d \n", (&s2.ptr[0]));
     system("pause");
-    DeleteArray(&arr);
+    return 0;
+
+
+    int out = 1;
+    Stack stack;
+    while (out != 0) {
+        stack = CreateStack(0);
+        if (stack.size == -1) {
+            MALLOC_ERROR;
+            printf("\n  Try again? 1/0: ");
+            while (ScanInt(&out) == 0 || (out != 0 && out != 1)) {
+                system("cls");
+                printf("\n  Try again? 1/0: ");
+            };
+            system("pause");
+            // ? DeleteArray(&arr);
+            return;
+        }
+        else {
+            Menu(&stack);
+            break;
+        };
+    }
+    system("pause");
+    DeleteStack(&stack);
     return 0;
 }
 
