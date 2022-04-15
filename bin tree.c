646c14475349f4767336a84c6573c58f7a1fbd04
arst0tzka <kaarst@ya.ru>
2022-04-15 22:28:39 +0300

    configured stack for tree, trying to realize print with stack cuz I have no clue how to do it in any other way

--- bin tree.c
+++ bin tree.c
@@ -7,6 +7,12 @@
 
 #define WHITE 15
 #define DARKGREY 8
+#define SUCCESS 1
+#define EMPTY_SIZE_ERROR -1 
+#define EMPTY_SIZE_MSG printf("\n  Error: not enough size for it!\n")
+#define REALLOC_ERROR -2
+#define REALLOC_MSG printf("\n  Error: realloc was finished with NULL ptr!\n")
+
 typedef struct Array {
     int data;
     struct Array* left;
@@ -14,6 +20,13 @@ typedef struct Array {
 } TreeStruct;
 typedef TreeStruct* TreeElem;
 typedef TreeElem* ElemPtr;
+
+
+typedef struct Array2 {
+    ElemPtr ptr;
+    int size;
+} Stack;
+typedef Stack* StackPtr;
  
 int ScanInt(int* valuePtr) {
     int scanCount = scanf("%d", valuePtr);
@@ -21,19 +34,6 @@ int ScanInt(int* valuePtr) {
     if (getc(stdin) != '\n') return 0;
     return 0;
 }
-
-
-TreeElem CreateElem(int val) {
-    TreeElem thisTree = (TreeElem)malloc(sizeof(TreeElem));
-    thisTree->data = val;
-    thisTree->left = NULL;
-    thisTree->right = NULL;
-    return thisTree;
-}
-void DeleteList(ElemPtr head) {
-    //
-}
-
 int GetValue() {
     int value = -1;
     printf("\n  Enter a value: ");
@@ -43,15 +43,146 @@ int GetValue() {
     };
     return value;
 }
-void Push(ElemPtr head) {
+
+
+Stack CreateStack() {
+    Stack thisStack;
+    thisStack.ptr = NULL;
+    thisStack.size = 0;
+    return thisStack;
+}
+int ReallocStack(StackPtr stack, int newSize) {
+    ElemPtr tmp_ptr = (ElemPtr)realloc(stack->ptr, newSize * sizeof(TreeElem));
+    if (tmp_ptr != NULL || newSize == 0) {
+        stack->ptr = tmp_ptr;
+        stack->size = newSize;
+    }
+    return (tmp_ptr != NULL || stack->size == 0);
+}
+void DeleteStack(StackPtr stack) {
+    stack->size = 0;
+    free(stack->ptr);
+    stack = NULL;
+}
+
+
+Stack GetPrimaryState(StackPtr stack) {
+    Stack currStack = CreateStack();
+    if (!ReallocStack(&currStack, stack->size))
+        return *stack;
+    for (int i = 0; i < stack->size; i++)
+        currStack.ptr[i] = stack->ptr[i];
+    return currStack;
+}
+TreeElem Pop(StackPtr stack) {
+    if (stack->size == 0) return NULL;
+    TreeElem topElem = stack->ptr[stack->size - 1];
+    ReallocStack(stack, stack->size - 1);
+    return topElem;
+}
+TreeElem Push(StackPtr stack, TreeElem newElem) {
+    ReallocStack(stack, stack->size + 1);
+    stack->ptr[stack->size - 1] = newElem;
+    return stack->ptr[stack->size - 1];
+}
+int WasError(int errorCode, StackPtr stack, StackPtr primaryStack) {
+    if (errorCode >= SUCCESS) return 0;
+    if (errorCode == REALLOC_ERROR) REALLOC_MSG;
+    if (errorCode == EMPTY_SIZE_ERROR) EMPTY_SIZE_MSG;
+
+    printf("%d, %d, %d", errorCode, stack->size, primaryStack->size);
+
+    DeleteStack(stack);
+    *stack = CreateStack();
+    stack->size = primaryStack->size;
+    for (int i = 0; i < stack->size; i++)
+        stack->ptr[i] = primaryStack->ptr[i];
+
+
+    *primaryStack = GetPrimaryState(stack);
+    if (primaryStack == stack) return REALLOC_ERROR;
+
+    return 1;
+}
+int StackEmpty(StackPtr stack) {
+    Stack thisStack = GetPrimaryState(stack);
+    if (Pop(&thisStack) == NULL) return 1;
+    return 0;
+}
+
+
+
+
+
+TreeElem CreateElem(int val) {
+    TreeElem thisTree = (TreeElem)malloc(sizeof(TreeStruct));
+    thisTree->data = val;
+    thisTree->left = NULL;
+    thisTree->right = NULL;
+    return thisTree;
+}
+void Print(ElemPtr head) {
+    if (*head == NULL) return;
+    //         10
+    //        /  \
+    //      6     13
+    //     / \      \ 
+    //    1   8      15
+    //         \     /
+    //          9   14 
+    
+
+    Stack ParentStack = CreateStack(); 
+    Stack ChildStack = CreateStack(); 
+    Push(&ParentStack, *head);
+    printf("  %d  ", StackEmpty(&ParentStack));
+    while (!StackEmpty(&ParentStack)) {
+        TreeElem elem = CreateElem(1);
+        while (elem != NULL) {
+            elem = Pop(&ParentStack);
+            if (elem->right != NULL) 
+                Push(&ChildStack, elem->right);
+            if (elem->left != NULL)
+                Push(&ChildStack, elem->left);
+            printf(" | %d | ", elem->data);
+        }
+        ParentStack = GetPrimaryState(&ChildStack);
+        ChildStack = CreateStack();
+    }
+    printf("\nhhh\n");
+}
+
+
+
+void DeleteList(ElemPtr head) {
+    //
+}
+
+
+TreeElem GoLeft(ElemPtr elem) {
+    if ((*elem)->left == NULL) return NULL;
+    return (*elem)->left;
+}
+TreeElem GoRight(ElemPtr elem) {
+    if ((*elem)->right == NULL) return NULL;
+    return (*elem)->right;
+}
+void PushTree(ElemPtr head) {
     int value = GetValue();
+    // check if value already in tree
     if (*head == NULL) {
-        *head = CreateElem(2);
+        *head = CreateElem(value);
         return;
     }
-
-
-    
+    TreeElem tElem = *head;
+    TreeElem nextElem = *head;
+    while (nextElem != NULL) {
+        tElem = nextElem;
+        if (tElem->data > value) nextElem = GoLeft(&tElem);
+        if (tElem->data < value) nextElem = GoRight(&tElem);
+    }
+    if (tElem->data > value) tElem->left = CreateElem(value);
+    if (tElem->data < value) tElem->right = CreateElem(value);
 }
 
 // General funcs
@@ -83,7 +214,7 @@ void Menu(ElemPtr head) {
         system("cls");
         switch (option) {
             case('1'): {
-                Push(head);
+                PushTree(head);
                 break;
             }
             case('2'): {
@@ -93,7 +224,7 @@ void Menu(ElemPtr head) {
             }
             case('3'): {
                 if (isEmpty) continue;
-                // FindMinMax(head);
+                Print(head);
                 break;
             }
             case('4'): {
